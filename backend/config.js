86df44ba06f1c2ee885d95e08ba3224cf930bd5d
Henzz <khenok53@gmail.com>
2024-09-26 22:27:15 +0300

    updating db cred

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT || 8080;
 export const mongoDBURL =
-  'mongodb+srv://khenok53:k63ARXmdlVg4RJMB@tuneuptopiacluster.lcbcn.mongodb.net/?retryWrites=true&w=majority&appName=tuneuptopiaCluster';
+  'mongodb+srv://tuneuptopia_user:f3bCQkYW6lnr3jQf@tuneuptopiacluster.lcbcn.mongodb.net/?retryWrites=true&w=majority&appName=tuneuptopiaCluster';
+// 'mongodb+srv://khenok53:k63ARXmdlVg4RJMB@tuneuptopiacluster.lcbcn.mongodb.net/?retryWrites=true&w=majority&appName=tuneuptopiaCluster';
